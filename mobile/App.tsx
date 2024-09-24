7e67ef1f7c4787b3e9050fa3c95f31c5d8db77d2
UnkindlyBiased <inoutoftheverge@gmail.com>
2024-09-24 15:01:43 +0300

    feat: mobile navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { MainPoint } from './src/main-point';
+import { HomeScreen } from './src/screens/home.screen';
+import { AboutScreen } from './src/screens/about.screen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
     return (
-        <MainPoint />
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="About" component={AboutScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 };
 
---
+++ mobile/src/screens/about.screen.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export const AboutScreen = ({ navigation }) => {
+    return (
+        <View style={styles.container}>
+            <Text>Some information</Text>
+            <Button
+                title="Go back"
+                onPress={() => navigation.goBack()} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
